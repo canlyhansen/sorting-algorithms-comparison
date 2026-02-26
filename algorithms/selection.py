d5275bc5f40d5a107969f32e6f733ff5b9735596
canlyhansen <canlyhansen@gmail.com>
2026-02-26 19:07:37 +0700

    Add selection sort function with array copy
    Implemented selection sort algorithm with a copy of the original array to avoid modification.

--- algorithms/selection.py
+++ algorithms/selection.py
@@ -1,1 +1,14 @@
-
+def selection_sort(arr): 
+    '''
+    For every iteration, we search for the smallest number in the array, then put it in front position.
+    Copy function is added to prevent the original data from any change.
+    '''
+    arr = arr.copy()
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i + 1, n):                    # i+1 is because this left part is ordered
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]  # order is switched, so that the front position is the smallest number
+    return arr
